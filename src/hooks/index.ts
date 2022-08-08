1604d69de07800eba3dc9aabb2378ea61514ff4a
kingkite <kitewebdevelopers@gmail.com>
2022-08-08 11:55:37 +0100

    Added usePagination Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
-export { default as useFadeIn } from "./useFadeIn";
-export { default as useOutClick } from "./useOutClick";
-export { default as useTabNavigator } from "./useTabNavigator";
+export { default as useFadeIn } from './useFadeIn';
+export { default as usePagination, PaginationType } from './usePagination';
+export { default as useOutClick } from './useOutClick';
+export { default as useTabNavigator } from './useTabNavigator';
